b01dfc15659473d8d9602e9944e08e44bdea118b
Oussama Bof <jackmarrow06@gmail.com>
2023-09-09 17:49:54 +0200

    add all the tables

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class EquipmentController extends Controller
 {
     public function index(){
-        return view('user.partials.equipments');
+        return view('admin.pages.equipments');
     }
 }
